8af7387dd72a2c635b7c1ed8af713e641ce756e6
Daniil Emelin <Daniil.Emelin@x5.ru>
2022-04-13 16:47:41 +0400

    fix posts utils

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -320,15 +320,17 @@ export async function getRelatedPosts(categories, postId, count = 5) {
     const filtered = posts.filter(({ postId: id }) => id !== postId);
     const sorted = sortObjectsByDate(filtered);
 
-    related.posts = sorted.map((post) => ({ title: post.title, slug: post.slug }));
+    if (related?.posts) {
+      related.posts = sorted.map((post) => ({ title: post.title, slug: post.slug }));
+    }
   }
 
-  if (!Array.isArray(related.posts) || related.posts.length === 0) {
+  if (!Array.isArray(related?.posts) || related?.posts?.length === 0) {
     related = await getRelatedPosts(categories, postId, count);
   }
 
-  if (Array.isArray(related.posts) && related.posts.length > count) {
-    return related.posts.slice(0, count);
+  if (Array.isArray(related?.posts) && related?.posts?.length > count) {
+    return related?.posts.slice(0, count);
   }
 
   return related;
